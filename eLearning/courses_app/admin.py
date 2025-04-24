df38515a443841cfd350d68be3e5fcc4af46e686
Jeena V <jeenav.valsan@gmail.com>
2025-04-24 08:32:23 +0400

    admin page updated

--- eLearning/courses_app/admin.py
+++ eLearning/courses_app/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import ContactMessage, Course,StudyMaterial,Enrollment ,Assignment
+from .models import ContactMessage, Course, Feedback, Question, Quiz, StudentSubmission,StudyMaterial,Enrollment ,Assignment
 
 admin.site.register(Course)
 admin.site.register(StudyMaterial)
 admin.site.register(Enrollment)
 admin .site.register(Assignment)
 admin.site.register(ContactMessage)
+admin.site.register(Quiz)
+admin.site.register(Feedback)
+admin.site.register(Question)
+admin.site.register(StudentSubmission)
+
+
+
+
